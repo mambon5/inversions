861510fda319229b82c6c85d95ae0d289c11ab31
roma <romdommas@gmail.com>
2024-05-01 15:18:59 +0200

    changed passwords

--- src/src/starter_all_stocks.sh
+++ src/src/starter_all_stocks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo barxelona | sudo -S /var/www/inversions/src/src/anal_tot > /var/www/inversions/output/output_anal_tot.log 2>&1
+echo password | sudo -S /var/www/inversions/src/src/anal_tot > /var/www/inversions/output/output_anal_tot.log 2>&1
--- src/src/starter_main_stocks.sh
+++ src/src/starter_main_stocks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo Barxelona_11 | sudo -S /var/www/inversions/src/src/anal_main > /var/www/inversions/output/sortida_main.log 2>&1
+echo password | sudo -S /var/www/inversions/src/src/anal_main > /var/www/inversions/output/sortida_main.log 2>&1
