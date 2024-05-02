45398ce705cc360b9591a1b04cf27ab02be3c01b
roma <romdommas@gmail.com>
2024-05-03 01:33:38 +0200

    changed index html

--- helper.js
+++ helper.js
@@ -1,30 +1,40 @@
 function escriuAccions() { // After the HTML content loads
-    var div = document.getElementById("llista");
-    var inner = "";
+   
 
-        let yourDate = new Date();
-    const offset = yourDate.getTimezoneOffset()
+    let yourDate = new Date();
+    
+    let data2 = yourDate;
+    data2.setDate(data2.getDate() - 1);
+
+    var avui = formatejaData(yourDate)
+    var ahir = formatejaData(data2)
+    
+    escriu_accio(avui);
+    escriu_accio(ahir);
+};
+
+function formatejaData(data) {
+   
+    const offset = data.getTimezoneOffset()
    
     yourDate = new Date(yourDate.getTime() - (offset*60*1000));
-    var avui = yourDate.toISOString().split('T')[0]
-    var filename = "output/stocks_slope_percent_"+avui + ".csv"
+    var dataForm = yourDate.toISOString().split('T')[0]
 
-    inner += "<a href='"+filename+"'> accions dia " + avui + " </a>";
+    return dataForm;
+}
+
+function escriu_accio(data) { // After the HTML content loads
+    let list = document.getElementById("llista");
+    var inner = "";
+
+    
+    var filename = "output/stocks_slope_percent_"+data + ".csv"
+
+    inner += "<a href='"+filename+"'> accions dia " + data + " </a>";
     
     let li = document.createElement('li');
     li.innerHTML =inner;
     list.appendChild(li); // Appends HTML to an element
 };
 
-// const path = require('path');
-// console.log(path.resolve("readMe.txt"));
 
-// var fs = require('fs');
-// var files = fs.readdirSync('/output/');
-
- let list = document.getElementById("llista");
-// for (i = 0; i < files.length; ++i) {
-//     let li = document.createElement('li');
-//     li.innerText = files[i];
-//     list.appendChild(li);
-// }
