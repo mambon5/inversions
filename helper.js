7b1e6ed161196cbaca4e3d5da1cd20e278c6d02f
roma <romdommas@gmail.com>
2024-10-08 21:03:04 +0200

    fixed bug in sorting main stock columns

--- helper.js
+++ helper.js
@@ -161,15 +161,15 @@ function ordenaMet1Array(arr1, arr2) {
 }
 
 function ordenaPerCol0(arr1, arr2) {
-  return arr1[0] > arr2[0]
+  return 1*arr1[0] > 1*arr2[0]
 }
 
 function ordenaPerCol1(arr1, arr2) {
-  return arr1[1] < arr2[1]
+  return 1*arr1[1] < 1*arr2[1]
 }
 
 function ordenaPerCol2(arr1, arr2) {
-  return arr1[2] < arr2[2]
+  return parseInt(arr1[2]) < parseInt(arr2[2])
 }
 
 function ordenaPerCol3(arr1, arr2) {
